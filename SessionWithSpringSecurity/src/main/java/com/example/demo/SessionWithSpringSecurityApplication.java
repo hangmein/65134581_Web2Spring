0bba9be9840b11b1f6ece4337381a5e20f3e5281
hangmein <vietnamesent@gmail.com>
2026-04-08 14:27:48 +0700

    sessionsecurity

---
+++ SessionWithSpringSecurity/src/main/java/com/example/demo/SessionWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionWithSpringSecurityApplication.class, args);
+	}
+
+}
