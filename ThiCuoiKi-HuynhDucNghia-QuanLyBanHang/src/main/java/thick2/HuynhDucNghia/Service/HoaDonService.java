ba96bc1569b2577aa3c8fad80b1265586991020e
hangmein <vietnamesent@gmail.com>
2026-06-01 14:50:27 +0700

    HoaDon
    HoaDonService

--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HoaDonService.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HoaDonService.java
@@ -40,47 +40,5 @@ public class HoaDonService {
         hoaDonRepo.deleteById(id);
     }
 
-    @Transactional
-    public String themDong(Integer maHoaDon, Integer maHangHoa, int soLuong) {
-        HoaDon hd = getById(maHoaDon);
-        HangHoa hh = hangHoaRepo.findById(maHangHoa).orElse(null);
-        if (hd == null || hh == null) return "Khong tim thay hoa don hoac hang hoa";
-        if (soLuong <= 0) return "So luong phai lon hon 0";
-        if (hh.getSoLuongTon() < soLuong) return "Khong du hang trong kho (con " + hh.getSoLuongTon() + ")";
 
-        ChiTietHoaDon ct = new ChiTietHoaDon();
-        ct.setHoaDon(hd);
-        ct.setHangHoa(hh);
-        ct.setSoLuong(soLuong);
-        ct.setDonGia(hh.getDonGia());
-        chiTietRepo.save(ct);
-        hd.getChiTiet().add(ct);   
-        hh.setSoLuongTon(hh.getSoLuongTon() - soLuong);
-        hangHoaRepo.save(hh);
-
-        capNhatTongTien(hd);
-        return null; 
-    }
-
-    @Transactional
-    public void xoaDong(Integer maChiTiet) {
-        ChiTietHoaDon ct = chiTietRepo.findById(maChiTiet).orElse(null);
-        if (ct == null) return;
-        HangHoa hh = ct.getHangHoa();
-        hh.setSoLuongTon(hh.getSoLuongTon() + ct.getSoLuong());
-        hangHoaRepo.save(hh);
-
-        HoaDon hd = ct.getHoaDon();
-        chiTietRepo.delete(ct);
-        hd.getChiTiet().removeIf(c -> c.getMaChiTiet().equals(maChiTiet));
-        capNhatTongTien(hd);
-    }
-    private void capNhatTongTien(HoaDon hd) {
-        BigDecimal tong = BigDecimal.ZERO;
-        for (ChiTietHoaDon c : hd.getChiTiet()) {
-            tong = tong.add(c.getThanhTien());
-        }
-        hd.setTongTien(tong);
-        hoaDonRepo.save(hd);
-    }
 }
