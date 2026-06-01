60fc807818581d76dd18b299f2f078eced00335d
hangmein <vietnamesent@gmail.com>
2026-06-01 16:23:49 +0700

    Thongke
    update thống kê cho dashbaord

--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/ThongKeLoaiHang.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/ThongKeLoaiHang.java
@@ -1,5 +1,21 @@
 package thick2.HuynhDucNghia.Service;
 
-public class ThongKeLoaiHang {
+import java.math.BigDecimal;
 
+public class ThongKeLoaiHang {
+    private final String tenLoai;
+    private final int soSanPham;
+    private final int tongTon;
+    private final BigDecimal giaTriTon;
+
+    public ThongKeLoaiHang(String tenLoai, int soSanPham, int tongTon, BigDecimal giaTriTon) {
+        this.tenLoai = tenLoai;
+        this.soSanPham = soSanPham;
+        this.tongTon = tongTon;
+        this.giaTriTon = giaTriTon;
+    }
+    public String getTenLoai() { return tenLoai; }
+    public int getSoSanPham() { return soSanPham; }
+    public int getTongTon() { return tongTon; }
+    public BigDecimal getGiaTriTon() { return giaTriTon; }
 }
