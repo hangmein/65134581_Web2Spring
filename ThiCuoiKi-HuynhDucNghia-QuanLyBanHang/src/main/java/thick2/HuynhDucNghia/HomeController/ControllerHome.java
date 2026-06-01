cc4e5d9d7cda9e66d3ef4610ddb785ebc40ec493
hangmein <vietnamesent@gmail.com>
2026-06-01 13:38:57 +0700

    HangHoa
    test getall hàng hóa nhưng tạch nên sửa bên properties bên application và đã thành công

--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/HomeController/ControllerHome.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/HomeController/ControllerHome.java
@@ -4,19 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 import jakarta.servlet.http.HttpSession;
+
 import thick2.HuynhDucNghia.Service.HangHoaService;
 
 @Controller
 public class ControllerHome {
-	@Autowired private HangHoaService hangHoaService;
+    
+    @Autowired 
+    private HangHoaService hangHoaService;
+
     // ---- Trang chu ----
     @GetMapping("/")
     public String home(HttpSession session, Model model) {
-    	int soHangHoa = hangHoaService.getAll().size();
-    	model.addAttribute("soHangHoa", soHangHoa);
+        // 1. Lấy dữ liệu thật của Hàng hóa
+        int soHangHoa = hangHoaService.getAll().size();
+        model.addAttribute("soHangHoa", soHangHoa);
+        
+        // 2. Truyền số 0 giả định cho Hóa đơn để tránh lỗi giao diện
+        model.addAttribute("soHoaDon", 0);
+        model.addAttribute("doanhThu", 0); 
+        
         return "index";
     }
 }
