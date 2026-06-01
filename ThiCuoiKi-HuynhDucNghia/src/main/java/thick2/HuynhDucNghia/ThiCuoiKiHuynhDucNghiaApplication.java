519d03717223c6535f93083314c650f549a648b1
hangmein <vietnamesent@gmail.com>
2026-06-01 09:08:20 +0700

    thi ck

---
+++ ThiCuoiKi-HuynhDucNghia/src/main/java/thick2/HuynhDucNghia/ThiCuoiKiHuynhDucNghiaApplication.java
@@ -0,0 +1,13 @@
+package thick2.HuynhDucNghia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiCuoiKiHuynhDucNghiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiCuoiKiHuynhDucNghiaApplication.class, args);
+	}
+
+}
