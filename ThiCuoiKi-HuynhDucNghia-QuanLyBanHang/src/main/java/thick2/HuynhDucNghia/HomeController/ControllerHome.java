299d1a6e8a1a32ba0a4777a6d3c62904f05a9d88
hangmein <vietnamesent@gmail.com>
2026-06-01 13:24:40 +0700

    HangHoaService
    fix lỗi HangHoaService

--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/HomeController/ControllerHome.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/HomeController/ControllerHome.java
@@ -1,19 +1,22 @@
 package thick2.HuynhDucNghia.HomeController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 import jakarta.servlet.http.HttpSession;
+import thick2.HuynhDucNghia.Service.HangHoaService;
 
 @Controller
 public class ControllerHome {
-
+	@Autowired private HangHoaService hangHoaService;
     // ---- Trang chu ----
     @GetMapping("/")
     public String home(HttpSession session, Model model) {
-
+    	int soHangHoa = hangHoaService.getAll().size();
+    	model.addAttribute("soHangHoa", soHangHoa);
         return "index";
     }
 }
--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HangHoaService.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HangHoaService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import thick2.HuynhDucNghia.Model.HangHoa;
 import thick2.HuynhDucNghia.Repo.HangHoaRepository;
 
+@Service
 public class HangHoaService {
 	 @Autowired
 	 private HangHoaRepository repo;
