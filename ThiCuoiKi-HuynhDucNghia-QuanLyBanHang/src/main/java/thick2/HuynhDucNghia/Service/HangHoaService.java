9c6b000670836b8491177281a341f2482a251c46
hangmein <vietnamesent@gmail.com>
2026-06-01 13:17:33 +0700

    HangHoaService
    update hàng hóa service getall và getid

--- ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HangHoaService.java
+++ ThiCuoiKi-HuynhDucNghia-QuanLyBanHang/src/main/java/thick2/HuynhDucNghia/Service/HangHoaService.java
@@ -1,5 +1,14 @@
 package thick2.HuynhDucNghia.Service;
 
-public class HangHoaService {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import thick2.HuynhDucNghia.Model.HangHoa;
+import thick2.HuynhDucNghia.Repo.HangHoaRepository;
 
+public class HangHoaService {
+	 @Autowired
+	 private HangHoaRepository repo;
+	 public List<HangHoa> getAll() { return repo.findAll(); }
+	 public HangHoa getById(Integer id) { return repo.findById(id).orElse(null); }
 }
